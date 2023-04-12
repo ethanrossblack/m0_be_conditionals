c8ca33ec6d19e44ca022f0a4068f9800255afed7
Ethan Black <ethanrossblack@gmail.com>
2023-04-12 12:14:29 -0600

    Add comments to Part 2 and a sidenote on cannibalism

--- comparisons.rb
+++ comparisons.rb
@@ -54,36 +54,66 @@ puts "Is number_students less than or equal to 21?", number_students <= 21, ""
 # this should print: true
 
 
-#-------------------
+#----------------------------------------
 # PART 2: Articulating what you are doing
-#-------------------
+#----------------------------------------
 
 # For the following prompts, you will be given a line of code and your task is to type out a Comment,
 # in English, explaining what that line of code is doing, including what the comparison will evaluate to.
 # Be as technically precise as possible, but don't just copy and paste a definition from the readings.
 # Make sure YOU can explain it that way!
 
+puts "----------------------------------------"," PART 2: Articulating what you are doing","----------------------------------------"
+
+# Print the result of the comparison: is 4 less than 9?
 puts 4 < 9
-#YOU DO: Explain.
+# This should print: true
 
+# Assign a value of 3 to the variable "books"
 books = 3
+# Print the result of the comparison: is 4 less than books?
 puts 4 < books
-# YOU DO: Explain.
+# This should print: false
 
+# Assign a value of 6 to the variable "friends"
 friends = 6
+# Assign a value of 2 to the variable "siblings"
 siblings = 2
+# Print the result of the comparison: is friends greater than siblings?
 puts friends > siblings
-# YOU DO: Explain.
+# This should print: true
 
+# Assign a value of 9 to the variable "attendees"
 attendees = 9
+# Assign a value of 8 to the variable "meals"
 meals = 8
+# Print the result of the comparison: is attendees not equal to meals?
 puts attendees != meals
-# YOU DO: Explain.
+# This should print: true
+
+#---------
+# SIDENOTE
+#---------
+# I would like to note that if this were a gathering of cannibals, attendees COULD be the meals. We don't know for certain.
+# Metaphysically, if this were the case, 'attendees != meals' would print: false.
+
+# If 1 attendee serves 8 cannibals, the cannibals could turn 1 attendee into 8 meals and reduce the number of total attendees by 1.
+# Numerically, based on the timeline of when/how meals are prepared, the statement would print: false, becasue we would have 8 attendees and 8 meals.
+
+# However, if these dinner-party cannibals view each solely in terms of meals, and not as fellow attendees, we could have a potential of 72 total meals.
+# We would have to answer the questions 
+#    1. Can an person simultaneously exist as 1 attendee and 8 potential meals?
+#    2. If not, what are the conditions under which 1 attendee becomes 8 meals?
+
+# I've already spent too much time on this sidenote, but maybe I'll express the sentiment in code one day once I learn more.
+#-------------
+# END SIDENOTE
+#-------------
 
 
-#-------------------
+#--------------------------
 # PART 3: Logical Operators
-#-------------------
+#--------------------------
 
 is_hungry = true
 finished_homework = false
